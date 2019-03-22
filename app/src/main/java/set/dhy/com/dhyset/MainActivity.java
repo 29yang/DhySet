f9682bbb138ea0e4022482742b4295ae9b3ea392
yang <d1066936839@163.com>
2019-03-22 09:26:07 +0800

    1

--- app/src/main/java/set/dhy/com/dhyset/MainActivity.java
+++ app/src/main/java/set/dhy/com/dhyset/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         initListener();
         StatusBarUtils.setStatusBar(this, "#ffffff", false);
     }
