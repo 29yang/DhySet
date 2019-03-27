52f5a900d414f9b7103bc99746094e48c687b95c
yang <d1066936839@163.com>
2019-03-27 16:04:28 +0800

    util

---
+++ utilscorelibrary/src/main/java/com/dhy/utilscorelibrary/ContextHolder.java
@@ -0,0 +1,24 @@
+package com.dhy.utilscorelibrary;
+
+import android.content.Context;
+
+/**
+ * application_context 工具类
+ */
+public class ContextHolder {
+    private static Context APPLICATION_CONTEXT;
+
+    /**
+     * 初始化context
+     */
+    public static void init(Context context) {
+        APPLICATION_CONTEXT = context.getApplicationContext();
+    }
+
+    /**
+     * application context
+     */
+    public static Context getContext() {
+        return APPLICATION_CONTEXT;
+    }
+}
